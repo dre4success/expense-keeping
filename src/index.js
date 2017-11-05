6405919a3d20ac874150105020e7ab8a7742d8d3
Damilare <dre4success@live.com>
2017-11-05 15:09:21 +0100

    Starter files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import App from './components/App';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>Starting afresh</p>, document.querySelector('#app'));
