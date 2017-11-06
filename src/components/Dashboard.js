648941d00dd04eb8ff8c6945339839b9a039bd3f
Damilare <dre4success@live.com>
2017-11-06 16:01:24 +0100

    setting up routes

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
+import ExpenseListFilter from './ExpenseListFilters';
+
+const Dashboard = () => (
+  <div>
+    <ExpenseListFilter />
+    <ExpenseList />
+  </div>
+)
+
+export default Dashboard;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from '../components/Header';
 import AddExpense from '../components/AddExpense';
 import HelpPage from '../components/HelpPage';
+import EditExpense from '../components/EditExpense';
+import NotFoundPage from '../components/NotFoundPage';
+import Dashboard from '../components/Dashboard';
 
 const AppRouter = () => (
   <BrowserRouter>
@@ -10,7 +13,13 @@ const AppRouter = () => (
       <Header />
     </div>
     <Switch>
-      <Route />
+      <Route exact path="/" component={Dashboard} />
+      <Route exact path="/create" component={AddExpense} />
+      <Route exact path="/edit/:id" component={EditExpense} />
+      <Route exact path="/help" component={HelpPage} />
+      <Route component={NotFoundPage} />
     </Switch>
   </BrowserRouter>
 )
+
+export default AppRouter;
