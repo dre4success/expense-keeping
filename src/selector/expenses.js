be7760b486924bda1b055651bbbebccff698e29c
Damilare <dre4success@live.com>
2017-11-07 00:41:42 +0100

    testing for expenses filters

--- src/selector/expenses.js
+++ src/selector/expenses.js
@@ -1,8 +1,7 @@
 import moment from 'moment';
 
 export default (expenses, { text, sortBy, startDate, endDate }) => {
-  return expenses
-    .filter(expense => {
+  return expenses.filter(expense => {
       const createdAtMoment = moment(expense.createdAt);
       const startDateMatch = startDate
         ? startDate.isSameOrBefore(createdAtMoment, 'day')
