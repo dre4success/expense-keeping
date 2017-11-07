4006b3c6c64bfb76838afda5094f5823410c2e25
Damilare <dre4success@live.com>
2017-11-07 23:49:49 +0100

    testing ExpenseListFilters

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -10,41 +10,43 @@ import {
   setEndDate
 } from '../actions/filter';
 
-class ExpenseListFilter extends Component {
+export class ExpenseListFilter extends Component {
   state = {
     calendarFocused: null
-  }
+  };
 
-  onDatesChange =({startDate, endDate}) => {
-    this.props.dispatch(setStartDate(startDate));
-    this.props.dispatch(setEndDate(endDate))
-  }
-  onFocusChange = (calendarFocused) => {
-    this.setState(() => ({calendarFocused}))
-  }
-  render() { 
-    return (  
+  onDatesChange = ({ startDate, endDate }) => {
+    this.props.setStartDate(startDate);
+    this.props.setEndDate(endDate);
+  };
+  onFocusChange = calendarFocused => {
+    this.setState(() => ({ calendarFocused }));
+  };
+  onTextChange = e => {
+    this.props.setTextFilter(e.target.value);
+  };
+
+  onSortChange = e => {
+    if (e.target.value === 'date') {
+      this.props.sortByDate();
+    } else if (e.target.value === 'amount') {
+      this.props.sortByAmount();
+    }
+  };
+
+  render() {
+    return (
       <div>
-        <input type="text"
+        <input
+          type="text"
           value={this.props.filters.text}
-          onChange={(e) => {
-            this.props.dispatch(setTextFilter(e.target.value))
-          }}
+          onChange={this.onTextChange}
         />
-        <select
-          value={this.props.filters.sortBy}
-          onChange={(e) => {
-            if(e.target.value === 'date') {
-              this.props.dispatch(sortByDate())
-            } else if(e.target.value === 'amount') {
-              this.props.dispatch(sortByAmount())
-            }
-          }}
-        >
+        <select value={this.props.filters.sortBy} onChange={this.onSortChange}>
           <option value="date">Date</option>
           <option value="amount">Amount</option>
         </select>
-        <DateRangePicker 
+        <DateRangePicker
           startDate={this.props.filters.startDate}
           endDate={this.props.filters.endDate}
           onDatesChange={this.onDatesChange}
@@ -55,14 +57,20 @@ class ExpenseListFilter extends Component {
           isOutsideRange={() => false}
         />
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    filters: state.filters
-  }
-}
- 
-export default connect(mapStateToProps)(ExpenseListFilter);
+const mapStateToProps = state => ({
+  filters: state.filters
+});
+
+const mapDispatchToProps = dispatch => ({
+  setTextFilter: text => dispatch(setTextFilter(text)),
+  sortByDate: () => dispatch(sortByDate()),
+  sortByAmount: () => dispatch(sortByAmount()),
+  setStartDate: startDate => dispatch(setStartDate(startDate)),
+  setEndDate: endDate => dispatch(setEndDate(endDate))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilter);
